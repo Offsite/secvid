4a1e12384f691f71bafcfd471a846735ca08e0f5
Richard <offsite_ric@hotmail.com>
2012-11-11 17:45:32 -0500

    first

---
+++ src/app/app.js
@@ -0,0 +1,20 @@
+Ext.application({
+    name: 'secvid',
+    
+    launch: function() {
+        Ext.create("Ext.Panel", {
+            fullscreen: true,
+            items: [
+                {
+                    xtype   : 'titlebar',
+                    docked  : 'top',
+                    title   : 'Monitor Matrix'
+                },
+                {
+                    xtype: 'video',
+                    url: 'http://192.168.0.195:8080/video.webm'
+                }
+            ]
+        })
+    }
+});
